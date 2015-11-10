957202449089c1c41ae948df946c629792ce4fd1
Kyle Weems <kyle@mindfly.com>
2015-11-10 13:43:01 -0800

    Support alias in doctype selection.

--- PreviewPicker/controllers/doctype.selector.controller.js
+++ PreviewPicker/controllers/doctype.selector.controller.js
@@ -15,15 +15,16 @@ angular.module("umbraco").controller("doctype.selector.controller", function($sc
 	* @description Gets all doc types via an API call, and builds a list of doctypes. If $scope.model.value matches one, then set that one as selected.
 	*/
 	$scope.buildDocTypeOptionList = function() {
-		docTypeSelectorResource.getAll().then(function(data) {
-			if (data && data.length) {
-				data.forEach(function(docType) {
+		docTypeSelectorResource.getAll().then(function(docTypes) {
+			if (docTypes && docTypes.length) {
+				docTypes.forEach(function(docType) {
 					var option =  {
+						alias: docType.alias,
 						id: docType.id,
 						name: docType.name
 					};
 					$scope.docTypes.push(option);
-					if ($scope.model.value == option.id) {
+					if ($scope.model.value == option.alias) {
 						$scope.selectedDocType = option;
 					}
 				});
@@ -36,7 +37,7 @@ angular.module("umbraco").controller("doctype.selector.controller", function($sc
 	* @description Sets the initial state for the variables.
 	*/
 	$scope.setVariables = function() {
-		$scope.docTypes = [{id: 0, name: "Select DocType"}];
+		$scope.docTypes = [{alias: '', id: 0, name: "Select DocType"}];
 		$scope.selectedDocType = $scope.docTypes[0];
 		$scope.buildDocTypeOptionList();
 	};
@@ -50,7 +51,7 @@ angular.module("umbraco").controller("doctype.selector.controller", function($sc
 	$scope.updateValue = function() {
 		var updatedValue = null;
 		if ($scope.selectedDocType.id != 0) {
-			updatedValue = $scope.selectedDocType.id;
+			updatedValue = $scope.selectedDocType.alias;
 		}
 		$scope.model.value = updatedValue;
 	};
